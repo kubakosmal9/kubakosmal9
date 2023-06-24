26fe203a3411f31c89074b37ed04e6830b671c3b
kubakosmal9 <Kubakosmal2@gmail.com>
2023-06-24 12:11:27 +0200

    making mobile wersion of description

--- src/components/mainDescription.vue
+++ src/components/mainDescription.vue
@@ -1,7 +1,10 @@
 <template>
     <div :style="{ opacity: animationEnd ? '1' : '0' }" class="cont">
         <div class="text">
-             <span class="title">Witaj</span> na mojej stronie internetowej! Jestem Jakub, początkujący 22-letni informatyk z doświadczeniem w dziale network engineeringu. Moją pasją jest tworzenie stron internetowych. Zapoznaj się z moim CV i historią zawodową. Chętnie podzielę się projektami, które zrealizowałem. Skontaktuj się ze mną, jeśli masz pytania lub zainteresowanie współpracą. Dziękuję za odwiedzenie strony!
+            <div class="text-wrapp">
+                <span class="title">Witaj</span> na mojej stronie internetowej! Jestem Jakub, początkujący 22-letni informatyk z doświadczeniem w dziale network engineeringu. Moją pasją jest tworzenie stron internetowych. Zapoznaj się z moim CV i historią zawodową. Chętnie podzielę się projektami, które zrealizowałem. Skontaktuj się ze mną, jeśli masz pytania lub zainteresowanie współpracą. Dziękuję za odwiedzenie strony!
+
+            </div>
         </div>
         <!-- <learn-more-button></learn-more-button>  -->
     </div>
@@ -33,26 +36,47 @@ export default{
     font-family:'Open Sans' ;
     flex-direction: column;
     position: absolute;
-    right: 15%;
-    transform: translateX(100%);
+    right: 0px;
+    transform: translateX(100%) ;
     display: flex;
-    flex-direction: column;
     transition: opacity 1s ease-in;
     justify-content: center;
-    width: 34%;
+    align-items: left;
+    font-size: 42px;
+    width: 50%;
     height: 50%;
     text-align: left;
     top: 50%;
     transform: translateY(-50%);
 }
 .title{
-    font-size: 42px;
+    font-size: 2em;
     margin-bottom: 10px;
     font-weight: 800;
 }
 .text{
-    font-size: 24px;
+    font-size: 0.5em;
     margin-top: 0;
-    line-height: 30px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 80%;
+    line-height: 1.4em;
+}
+
+@media screen and (max-width: 400px) {
+
+.cont{
+    width: 100%;
+    height: 50%;
+    position: absolute;
+    font-size: 27px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+    
 }
 </style>
